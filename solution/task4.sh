3993f823c72b7032e7db2daadc4b56eeb6689d5a
Nick-Dvesty <genmeln05@gmail.com>
2024-12-10 09:50:25 +0700

    fix2

--- solution/task4.sh
+++ solution/task4.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
+sed 's/Живи/Не спи/g; s/Умрёшь/Уснёшь/g' data/blok.txt > data/blok_kids_edition.txt
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <number>"
-    exit 1
-fi
-
-n=$1
-
-for ((i=1; i<=n; i++)); do
-    if ((i % 3 == 0)) && ((i % 5 == 0)); then
-        echo "Fizz Buzz"
-    elif ((i % 3 == 0)); then
-        echo "Fizz"
-    elif ((i % 5 == 0)); then
-        echo "Buzz"
-    else
-        echo $i
-    fi
-done
